f9b2c928d41b6e65a3df81cd5cb6b7398354ea06
Melvin George <melvingeorge10@gmail.com>
2018-02-24 19:41:17 +0530

    heha

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use('/',indexRoutes);
 //listening on port 3000 (locally) or process.env.PORT;
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT,()=>{
+app.listen(process.env.PORT || 3000,()=>{
   console.log(`Server Started on PORT : ${PORT}`);
 });
