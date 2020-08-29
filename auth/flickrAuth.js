b42f75532f1b6046eccdc079f6fccc961abdc82f
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-08-30 01:39:26 +0530

    bug fixes

--- auth/flickrAuth.js
+++ auth/flickrAuth.js
@@ -1,17 +1,17 @@
-const keys = require('./keys'),
-      passport = require('passport'),
-      oauthFlickr = require('passport-oauth').OAuthStrategy;
+const passport = require("passport"),
+  oauthFlickr = require("passport-oauth").OAuthStrategy;
 
-passport.use('flickr',new oauthFlickr({
-  requestTokenURL:'https://www.flickr.com/services/oauth/request_token',
-  accessTokenURL:'https://www.flickr.com/services/oauth/access_token',
-  userAuthorizationURL:'https://www.flickr.com/services/oauth/authorize',
-  consumerKey:keys.clientKey,
-  consumerSecret:keys.clientSecret,
-  callbackURL:'/auth/provider/callback'
-},
-(token,tokenSecret,profile,done)=>{
-  console.log(token);
-  done();
-}
-));
+passport.use(
+  "flickr",
+  new oauthFlickr({
+    requestTokenURL: "https://www.flickr.com/services/oauth/request_token",
+    accessTokenURL: "https://www.flickr.com/services/oauth/access_token",
+    userAuthorizationURL: "https://www.flickr.com/services/oauth/authorize",
+    consumerKey: process.env.clientKey,
+    consumerSecret: process.env.clientSecret,
+    callbackURL: "/auth/provider/callback",
+  }, (token, tokenSecret, profile, done) => {
+    console.log(token);
+    done();
+  }),
+);
--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -1,11 +1,13 @@
-const auth = require('../auth/keys');
-const request = require('request');
-const photoSearch = (req,res)=>{
-  request(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${auth.clientKey}&text=${req.params.text}&format=json&nojsoncallback=1`,(e,resp,body)=>{
-    res.send(body);
-  });
+const request = require("request");
+const photoSearch = (req, res) => {
+  request(
+    `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.clientKey}&text=${req.params.text}&format=json&nojsoncallback=1`,
+    (e, resp, body) => {
+      res.send(body);
+    },
+  );
 };
 
 module.exports = {
-  photoSearch : photoSearch
+  photoSearch: photoSearch,
 };
